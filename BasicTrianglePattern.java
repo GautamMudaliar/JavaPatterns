b4f5c2bd8c93b662d9ece512ee8a6c215ef9f270
Gautam Mudaliar <gautammudliar@gmail.com>
2025-05-26 22:01:35 -0500

    changes to BasicTrianglePattern

--- BasicTrianglePattern.java
+++ BasicTrianglePattern.java
@@ -5,10 +5,10 @@ class BasicTrianglePattern {
     public static void main(String[] args) {
         
         int rows = 4;                 
-        int columns = 5;  
+        //int columns = 5; not relevant  
         
         for (int j = 0 ; j < rows ; j++){
-        for (int i =0 ; i<= j && i < columns ; i ++){
+        for (int i =0 ; i<= j ; i ++){
             
             System.out.print("*");
         }
